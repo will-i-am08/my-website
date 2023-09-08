f6a8a931e045ddb7d1d1fda29bf7a2c32742675f
Will <will@jmcalder.com>
2023-09-08 22:00:27 +1000

    added contact us

--- app/page.tsx
+++ app/page.tsx
@@ -21,11 +21,13 @@ export default function Home() {
           Welcome to William Calder Computers 
         </h1>
         <p className="text-lg">
-          Operating System Install building office and Gaming PCs fixing broken computers
-          and seting up tech.
+          this is my small tech company that i am starting up and will expand on in the future, if you are 
+        </p>
+        <p className="text-lg">
+        interested in any of our services give us a call and we can give you some prices  
         </p>
         <h2 className="text-2xl font-bold mb-4 mt-16">
-          What We Offer
+          What We Offer 
         </h2>
         <li className="text-md">
           PC Repairs 
@@ -48,12 +50,26 @@ export default function Home() {
         <li className="text-md">
           Operating System Install
         </li>
+        <li className="text-md">
+          Operating System Uptating 
+        </li>
+        <li className="text-md">
+          Office PCs and Gaming PCs
+        </li>
         <p className="text-lg mt-6">
           If there somthing that you need and is not listed here and just give me a call and tell you if i can do it or not 
         </p>
         <h2 className="text-2xl font-bold mb-4 mt-16">
-          What We Offer
+          Contact Us 
         </h2>
+        <p className="text-lg mt-2">
+          <a href='mailto:will@williamcalder.com.au'>
+          Email - will@williamcalder.com.au
+          </a>
+        </p>
+        <p className="text-lg mt-2">
+          Phone - 0480 436 685 
+        </p>
         </div>
       </div>
     </main>
